afbded8b63b00b4102850cec9defef005a853f55
Zakwan601 <zakwan6012003@gmail.com>
2025-07-11 18:59:08 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,1 @@
-import requests
-
-url = "https://www.playmartbd.com/"
-
-try:
-    response = requests.get(url)
-    print("Pinged site! Status code:", response.status_code)
-except Exception as e:
-    print("Error:", e)
+print("Hello, GitHub Actions!")
